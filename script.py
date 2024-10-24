03d505891cdab6b7838673d4e82c5a4c0639c874
Sejalpandey17 <2333026@sliet.ac.in>
2024-10-24 13:13:03 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,10 +7,10 @@ from datetime import datetime, timedelta
 import pytz
 import requests
 
-username = "anwarchegg05@gmail.com"
-password = "Author@05"
+username = "sunitha05chegg@gmail.com"
+password = "Chegg@050402"
 user_bot_chatID = '1155462778'
-account_name = "Anwar"
+account_name = "Sunita"
 accept_option = True
 start_time = 0                   #Stating time. Default 0. In 24 hour format
 end_time = 25                    #Stating time. Default 25. In 24 hour format
